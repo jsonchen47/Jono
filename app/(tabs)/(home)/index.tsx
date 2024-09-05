34ec09e14859755502d79255f581bcf4f32afa86
JasonChen47 <jwc297@cornell.edu>
2024-09-05 11:31:10 -0700

    added the newly added projects

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -2,12 +2,72 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { Link } from 'expo-router';
-import { View, StyleSheet, Image, ScrollView, Animated } from 'react-native';
+import { View, StyleSheet, Image, ScrollView, Animated, Dimensions } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { Card, Text } from '@ui-kitten/components';
+import { tSImportEqualsDeclaration } from '@babel/types';
 
 // const av = new Animated.Value(0);
 // av.addListener(() => {return});
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+type IconName = 
+  | 'checkmark-circle'
+  | 'book-outline'
+  | 'leaf-outline'
+  | 'medkit-outline'
+  | 'cash-outline'
+  | 'body-outline'
+  | 'cog-outline'
+  | 'globe-outline'
+  // Add other icon names here
+  ;
+
+interface Category {
+  id: number;
+  name: string;
+  icon: IconName;
+}
+
+const sampleCategories: Category[] = [
+  {
+    id: 1, 
+    icon: "medkit-outline",
+    name: 'Health',
+  },
+  {
+    id: 2, 
+    icon: "book-outline",
+    name: 'Education',
+  }, 
+  {
+    id: 3, 
+    icon: "leaf-outline",
+    name: 'Environment',
+  },
+  {
+    id: 4, 
+    icon: "cash-outline",
+    name: 'Finance',
+  },
+  {
+    id: 5, 
+    icon: "body-outline",
+    name: 'Social Justice',
+  },
+  {
+    id: 6, 
+    icon: "cog-outline",
+    name: 'Technology',
+  },
+  {
+    id: 7, 
+    icon: "globe-outline",
+    name: 'Politics',
+  },
+]
 
 const sampleProjects = [
   {
@@ -69,10 +129,37 @@ const Tab = createMaterialTopTabNavigator();
 export default function ExploreScreen() {
   return (
     <View style={styles.container}>
-      <ScrollView horizontal={true}>
-        <Ionicons name="checkmark-circle" size={32} />
-        <Ionicons name="checkmark-circle" size={32} />
+      <ScrollView>
+        <ScrollView horizontal={true}>
+          {/* <Ionicons name="checkmark-circle" size={32} />
+          <Ionicons name="checkmark-circle" size={32} /> */}
+          {sampleCategories.map((category) => (
+          <View style={styles.categories} key={category.id} >
+            {/* Image */}
+            <Ionicons name={category.icon} size={32} style={styles.icon} />
+            
+            {/* Text */}
+            <Text style={styles.categoryLabel}>{category.name}</Text>
+          </View>
+          ))}
+        </ScrollView>
+        {/* Newly added projects */}
+        <Text style={styles.headerText}>Newly Added</Text>
+        <ScrollView horizontal>
+        {sampleProjects.map((project) => (
+        <View key={project.id} style={styles.largeProjectsView} >
+          {/* Image */}
+          <Image 
+            style={styles.largeProjectsImages}
+            source={project.image} 
+          />
+          {/* Text */}
+          <Text>{project.title}</Text>
+        </View>
+        ))}
+        </ScrollView>
       </ScrollView>
+      
     </View>
   );
 }
@@ -82,9 +169,30 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-start',
     alignItems: 'flex-start',
-    padding: 20,
   },
-  horizontalRow: {
-    flexDirection: 'row', 
+  categories: {
+    padding: 15,
+    flex: 1,
+    alignItems: 'center',
+  },
+  icon: {
+    marginBottom: 5,
+  },
+  categoryLabel: {
+    fontSize: 12,
+  },
+  headerText: {
+    fontSize: 20,
+    paddingLeft: 20,
+    paddingTop: 10,
+    fontWeight: 'bold',
+  },
+  largeProjectsView: {
+    padding: 15,
+  },
+  largeProjectsImages: {
+    width: windowWidth/2, 
+    height: windowHeight/5,
+    paddingBottom: 15,
   }
 });
