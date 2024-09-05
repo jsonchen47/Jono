e4213a07a791c9245641a5101f3b869eb5769937
JasonChen47 <jwc297@cornell.edu>
2024-09-05 09:43:11 -0700

    installed UI kitten and played around with UI alignment

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -2,21 +2,57 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { Link } from 'expo-router';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Card, Text } from '@ui-kitten/components';
 
+const sampleProjects = [
+  {
+    id: 1,
+    title: 'John Doe',
+    image: require('../../../assets/images/chair.png'),
+    author: 'Engineer',
+  },
+  {
+    id: 2,
+    title: 'John Doe',
+    image: require('../../../assets/images/chair.png'),
+    author: 'Engineer',
+  },
+];
 
-function HomeScreen() {
+function ProjectsScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
+    <View style={styles.container}>
+      {/* <Text>Projects!</Text> */}
+      {/* <Ionicons name="checkmark-circle" size={32} /> */}
+
+      {sampleProjects.map((project) => (
+        <View key={project.id} >
+          {/* Image */}
+          <Image 
+            style={{width: 50, height: 50}}
+            source={project.image} 
+          />
+          {/* Text */}
+          <Text>{project.title}</Text>
+        </View>
+      ))}
+        {/* <Card>
+          <Text>
+            The Maldives, officially the Republic of Maldives, is a small country in South Asia,
+            located in the Arabian Sea of the Indian Ocean.
+            It lies southwest of Sri Lanka and India, about 1,000 kilometres (620 mi) from the Asian continent
+          </Text>
+        </Card> */}
     </View>
   );
 }
 
-function SettingsScreen() {
+function PeopleScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+      <Text>People!</Text>
     </View>
   );
 }
@@ -26,8 +62,8 @@ const Tab = createMaterialTopTabNavigator();
 export default function ExploreScreen() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Screen name="Projects" component={ProjectsScreen} />
+      <Tab.Screen name="People" component={PeopleScreen} />
     </Tab.Navigator>
   );
 }
@@ -35,7 +71,8 @@ export default function ExploreScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    padding: 20,
   },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,23 @@
 import { Stack } from 'expo-router';
+import { PaperProvider } from 'react-native-paper';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen 
-        name="(tabs)" 
-        options={{
-          // Hide the header for this route
-          headerShown: false,
-        }}
-      />
-    </Stack>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <PaperProvider>
+        <Stack>
+          <Stack.Screen 
+            name="(tabs)" 
+            options={{
+              // Hide the header for this route
+              headerShown: false,
+            }}
+          />
+        </Stack>
+      </PaperProvider>
+    </ApplicationProvider>
   );
 }
 
