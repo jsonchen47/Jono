8377f2951b4dde78551fae143c6a7c607e0a1a5b
JasonChen47 <jwc297@cornell.edu>
2024-09-22 08:46:47 -0700

    started integrating auth with database

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,15 +6,40 @@ import {NavigationContainer} from '@react-navigation/native';
 import Navigator from '../src/navigation'
 import ChatScreen from './ChatScreen'
 import { View, StyleSheet, ScrollView, Image, Dimensions, SafeAreaView, ListRenderItem } from 'react-native';
-import { Amplify } from "aws-amplify"; 
+import { Amplify, Auth, API, graphqlOperation } from "aws-amplify"; 
 // @ts-ignore
 import {withAuthenticator} from "aws-amplify-react-native"; 
 import awsconfig from "../src/aws-exports";
 // import {Picker} from 'react-native';
+import { useEffect } from 'react'; 
+import {getUser} from '../src/graphql/queries'
 
 Amplify.configure({ ...awsconfig, Analytics: {disabled: true}});
 
 function RootLayout() {
+
+useEffect(() => {
+  
+  const syncUser = async () => {
+    // get Auth user
+    const authUser = await Auth.currentAuthenticatedUser({
+      bypassCache: true,
+    });
+    console.log(authUser);
+
+    // query the database using Auth user id (sub)
+    const userData = await API.graphql(
+      graphqlOperation(getUser, {id: authUser.attributes.sub})
+      );
+      console.log(userData)
+
+    // if there are no users in db, create one 
+  };
+
+  syncUser();
+  
+}, []);
+
   return (
     <ApplicationProvider {...eva} theme={eva.light}>
       <PaperProvider>
