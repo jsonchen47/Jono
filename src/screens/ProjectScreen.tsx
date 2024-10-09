cc300c2a26286141968cda74fc07c6a2a916322b
JasonChen47 <jwc297@cornell.edu>
2024-10-09 11:51:04 -0700

    added image and title to the project screen

--- src/screens/ProjectScreen.tsx
+++ src/screens/ProjectScreen.tsx
@@ -1,14 +1,25 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, Dimensions, StyleSheet } from 'react-native'
 import React from 'react'
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const ProjectScreen = ( {project }: any ) => {
   return (
     <View>
-        <Image style={{width: 300, height: 300}} source={{uri: project?.image}}/>
-        <Text>{project?.title}</Text>
+        <Image style={{width: windowWidth, height: windowHeight/3}} source={{uri: project?.image}}/>
+        <Text style = {styles.title} >{project?.title}</Text>
         <Text>{project?.author}</Text>
     </View>
   )
 }
 
 export default ProjectScreen
+
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 25, 
+        fontWeight: 'bold', 
+        padding: 15, 
+    }
+})
