3724ac7c87624c701c6fd16fff2160b41d4f760d
JasonChen47 <jwc297@cornell.edu>
2024-10-21 16:07:06 -0700

    tried adding search capabilities

--- app/search.tsx
+++ app/search.tsx
@@ -1,13 +1,58 @@
-import { View, Text, TextInput, SafeAreaView, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, FlatList, TextInput, SafeAreaView, StyleSheet, Dimensions } from 'react-native';
 import React, { useState } from 'react';
 import { useRouter } from 'expo-router';
 import Icon2 from 'react-native-vector-icons/Ionicons';
 import Icon3 from 'react-native-vector-icons/AntDesign';
+import { API, graphqlOperation } from 'aws-amplify';
+import { searchProjects } from '@/src/graphql/queries';
+import { GraphQLResult } from '@aws-amplify/api-graphql';
+
 
 const windowWidth = Dimensions.get('window').width;
 
+const ResultsList = ({ results }: any) => {
+  return (
+    <FlatList
+      data={results}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <View>
+          <Text>{item.name}</Text>
+          <Text>{item.description}</Text> {/* Display additional info if needed */}
+        </View>
+      )}
+    />
+  );
+};
+
 const Search = () => {
   const [text, setText] = useState('');
+  const [results, setResults] = useState([]); 
+
+  const fetchSearchResults = async (searchTerm: any) => {
+    try {
+      const formattedSearchTerm = `*${searchTerm}*`
+      const searchResult = await API.graphql(graphqlOperation(searchProjects, {
+        filter: {
+          title: {
+            // wildcard: formattedSearchTerm
+            match: searchTerm
+          }
+        }
+      }));
+      
+      const castedSearchResult = searchResult as GraphQLResult<any>
+      console.log(castedSearchResult)
+      const items = castedSearchResult?.data?.searchProjects?.items || [] // grab the search results
+      console.log(items)
+      setResults(items); // store the results in state
+    } catch (error) {
+      console.error('Error searching: ', error);
+    }
+  };
+  const handleSearch = () => {
+    fetchSearchResults(text);
+  };
   const router = useRouter();
 
   return (
@@ -27,9 +72,11 @@ const Search = () => {
             autoFocus={true}
             value={text}
             onChangeText={setText}
+            onSubmitEditing={handleSearch}
           />
         </View>
       </View>
+      <ResultsList results={results} />
     </SafeAreaView>
   );
 };
